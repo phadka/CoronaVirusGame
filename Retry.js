0b206d30d12d695cccdc86deb48c1c62d07aa514
phadka <62934210+phadka@users.noreply.github.com>
2020-04-01 21:07:55 +0200

    Update Retry.js

--- Retry.js
+++ Retry.js
@@ -6,13 +6,13 @@ class Retry {
         this.bg.draw();
         drawText('Punktzahl: ' + points, 800, 400, 50, true);
         if (mouseY > 505 && mouseY < 595 && mouseX > 400 && mouseX < 1200) {
-            drawText('Nochmal Versuchen', 800, 550, 90, true, 'grey');
+            drawText('Nochmal versuchen', 800, 550, 90, true, 'grey');
             if (mouseClick) {
                 mode = 'endless';
                 points = 0;
             }
         } else {
-            drawText('Nochmal Versuchen', 800, 550, 90, true);
+            drawText('Nochmal versuchen', 800, 550, 90, true);
         }
     }    
 }
